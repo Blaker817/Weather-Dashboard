a20578c45c93a6217ac655246adc01c8ce0c58d0
Blake Elliott <blakeelliott@Blakes-MacBook-Pro.local>
2022-01-19 09:47:02 -0600

    added humidity and wind

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,54 +3,59 @@ var searchBtn = document.querySelector('.search-btn')
 var fiveContainer = document.querySelector('.five-day-container')
 
 
-searchBtn.addEventListener('click', function() {
+searchBtn.addEventListener('click', function () {
     var searchedCity = document.querySelector('.input-value').value
     getCurrentWeather(searchedCity)
 })
 
 function getCurrentWeather(value) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + value + '&units=imperial&appid=' + API_KEY)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
 
-        var lat = data.coord.lat
-        var lon = data.coord.lon
-        getFiveDayWeather(lat, lon)
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+            getFiveDayWeather(lat, lon)
 
-        document.querySelector('.city-name').textContent = data.name
-        document.querySelector('.temp').textContent = 'Temp: ' + data.main.temp + ' F'
-        //hum
-
-        //wind
-    })
+            document.querySelector('.city-name').textContent = data.name
+            document.querySelector('.temp').textContent = 'Temp: ' + data.main.temp + ' F'
+            //hum
+            document.querySelector('.humidity').textContent = data.main.humidity
+            //wind
+            document.querySelector('.wind').textContent = data.wind.speed
+        })
 }
 
 function getFiveDayWeather(lat, lon) {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&units=imperial&appid='+ API_KEY)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + API_KEY)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
 
-        document.querySelector('.uvi').textContent = 'UV Index: ' + data.current.uvi
+            document.querySelector('.uvi').textContent = 'UV Index: ' + data.current.uvi
 
-        for (var i = 0; i < 5; i++) {
-            var card = document.createElement('div')
-            fiveContainer.append(card)
+            for (var i = 0; i < 5; i++) {
+                var card = document.createElement('div')
+                fiveContainer.append(card)
 
-            var date = document.createElement('h2')
-            date.textContent = moment().add(i + 1, 'days').format('dddd')
-            card.append(date)
+                var date = document.createElement('h2')
+                date.textContent = moment().add(i + 1, 'days').format('dddd')
+                card.append(date)
 
-            var fiveTemp = document.createElement('p')
-            fiveTemp.textContent = 'Temp: ' + data.daily[i + 1].temp.day + ' F'
-            card.append(fiveTemp)
-            
-            var fiveHum = document.createElement('p')
-            fiveHum.textContent = 'Humidity: ' + data.daily[i + 1].humidity
-            card.append(fiveHum)
+                var fiveTemp = document.createElement('p')
+                fiveTemp.textContent = 'Temp: ' + data.daily[i + 1].temp.day + ' F'
+                card.append(fiveTemp)
 
-            //get the wind
-        }
-    })
+                var fiveHum = document.createElement('p')
+                fiveHum.textContent = 'Humidity: ' + data.daily[i + 1].humidity
+                card.append(fiveHum)
+
+                //get the wind
+                var fiveWind = document.createElement('p')
+                fiveWind.textContent = 'Wind: ' + data.daily[i + 1].wind_speed
+                card.append(fiveWind)
+
+            }
+        })
 }
